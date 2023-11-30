b66341387c2e7c9ec527e9b6c2c8317c37bb0a84
Faithfulness Obasi <faithful1ofall@gmail.com>
2023-11-30 09:00:44 -0800

    done

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,8 +2,7 @@
 
 def magic_calculation(a, b):
     """Match bytecode"""
-"""    from magic_calculation_102 import add, sub """
-add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
+    add, sub = __import__('magic_calculation_102', fromlist=('add', 'sub'))
 
     if a < b:
         c = add(a, b)
