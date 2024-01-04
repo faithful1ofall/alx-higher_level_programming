95e648f2201a56b6c9e4398ddf9aeeba659ddd56
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-01-04 15:27:46 +0100

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,9 +5,9 @@ Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
 
 Example:
-    $ ./101-nqueens.py I
+    $ ./101-nqueens.py N
 
-I must be an integer greater than or equal to 4.
+N must be an integer greater than or equal to 4.
 
 Attributes:
     board (list): A list of lists representing the chessboard.
