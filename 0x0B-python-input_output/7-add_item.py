8e94264d7b8583bd897aedc4ff3f24546b7840c2
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-01-09 15:02:37 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""import json for encoding and decoding the json file
-import sys to help parse through the arguments
-import os.path to help check if the file exists"""
-import json
-import os.path
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
-"""importing the necessary modules to help in craeting object from
-JSON file and write an object text file using json representation"""
-enc = __import__('5-save_to_json_file').save_to_json_file
-dec = __import__('6-load_from_json_file').load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-my_list = []
-filename = "add_item.json"
-
-if os.path.isfile(filename):
-    """decode the json file if the file exists"""
-    my_list = dec(filename)
-
-"""loop through the list of arguments"""
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-enc(my_list, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
